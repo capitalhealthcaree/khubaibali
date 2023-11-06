925e8d1a507da14e9c96124b52135433636255db
capitalhealthcaree <premierpatientappointment@gmail.com>
2023-11-06 14:34:40 -0800

    remove tags

--- src/partials/blog/index.jsx
+++ src/partials/blog/index.jsx
@@ -6,7 +6,6 @@ import Link from 'next/link'
 import 'swiper/css'
 import 'swiper/css/pagination'
 import { Card, Col, Row } from 'react-bootstrap'
-// import { capitalizeFirstLetter } from 'root/utils'
 import { cx } from '@emotion/css'
 import dayjs from 'dayjs'
 import styled from './style'
@@ -14,25 +13,6 @@ import styled from './style'
 const Post = ({ data }) => {
   // Destructure passed data
   const { title, date, summary, imageUrl, slug } = data
-
-  // Populates the blog post card with passed tags
-  // const tagsToText = (array) => {
-  //   // Capitalizes first letter of each tag
-  //   const treatedArray = array.map((element) => capitalizeFirstLetter(element))
-
-  //   // Checks length for singular/plural formatting
-  //   if (treatedArray.length === 1)
-  //     return <a className='link'>{treatedArray[0]}</a>
-
-  //   // Joins tags for plural display
-  //   return treatedArray.reduce((prev, curr) => (
-  //     <>
-  //       <span className='_tag'>{prev}</span>
-  //       <span className='_delimiter'>/</span>
-  //       <span className='_tag'>{curr}</span>
-  //     </>
-  //   ))
-  // }
 
   // Format passed plain text date
   const dateToText = (dateInput) => dayjs(dateInput).format('MMMM D, YYYY')
@@ -66,10 +46,6 @@ const Post = ({ data }) => {
           <Card.Title className='_title'>{title}</Card.Title>
         </Link>
         <Card.Text className='_summary'>{summary}</Card.Text>
-        <div className='_tags'>
-          <span className='_key'>Tags: </span>
-          {/* <span className='_list'>{tagsToText(tags)}</span> */}
-        </div>
       </Card.Body>
     </Card>
   )
@@ -83,7 +59,7 @@ const PostsList = () => {
   const fetchedData = [
     {
       title: 'sdas00',
-      tags: ['ReactJs', 'NextJs'],
+
       summary: 'It is very easy',
       slug: 'reactjs-is-happy',
       imageUrl:
@@ -91,7 +67,7 @@ const PostsList = () => {
     },
     {
       title: 'sdas00',
-      tags: ['ReactJs', 'NextJs'],
+
       summary: 'It is very easy',
       slug: 'reactjs-is-happy',
       imageUrl:
@@ -99,7 +75,7 @@ const PostsList = () => {
     },
     {
       title: 'sdas00',
-      tags: ['ReactJs', 'NextJs'],
+
       summary: 'It is very easy',
       slug: 'reactjs-is-happy',
       imageUrl:
@@ -107,7 +83,7 @@ const PostsList = () => {
     },
     {
       title: 'sdas00',
-      tags: ['ReactJs', 'NextJs'],
+
       summary: 'It is very easy',
       slug: 'reactjs-is-happy',
       imageUrl:
@@ -115,7 +91,7 @@ const PostsList = () => {
     },
     {
       title: 'sdas00',
-      tags: ['ReactJs', 'NextJs'],
+
       summary: 'It is very easy',
       slug: 'reactjs-is-happy',
       imageUrl:
@@ -123,7 +99,7 @@ const PostsList = () => {
     },
     {
       title: 'sdas00',
-      tags: ['ReactJs', 'NextJs'],
+
       summary: 'It is very easy',
       slug: 'reactjs-is-happy',
       imageUrl:
