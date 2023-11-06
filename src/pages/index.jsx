2b8bc72f1c9846ce6af6cb3bf68800e156885064
capitalhealthcaree <premierpatientappointment@gmail.com>
2023-11-06 14:05:25 -0800

    update footer

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 /*
-This is the home page but with just an image background for the Hero section
+This is the home page but with a video background for the Hero section
 */
 
 import ScrollWrapper from 'root/src/components/scroll-wrapper'
@@ -7,7 +7,6 @@ import Hero from 'root/src/partials/hero'
 import About from 'root/src/partials/about'
 import Services from 'root/src/partials/services'
 import Hire from 'root/src/partials/hire'
-import Portfolio, { PortfolioDataPath } from 'root/src/partials/portfolio'
 import Blog, { BlogDataPath } from 'root/src/partials/blog'
 import Contact from 'root/src/partials/contact'
 import Footer from 'root/src/partials/footer'
@@ -15,20 +14,19 @@ import parseAllMdx from 'root/src/lib/parseAllMdx'
 import Metadata from 'root/src/metadata'
 import Testimonials, { testimonialsDataPath } from '../partials/testimonials'
 
-const HomeImage = ({ portfolioData, testimonialsData, blogData }) => (
+const HomeVideo = ({ testimonialsData, blogData }) => (
   /* Wrap all sections within a scroll-wrapper that adds a functional navbar/sidebar feature */
   <ScrollWrapper>
     {/* Include website metadata */}
     <Metadata />
 
-    {/* Hero section with an image background */}
-    <Hero nav='Home' id='home' variant='image' />
+    {/* Hero section with a video background */}
+    <Hero nav='Home' id='home' variant='video' />
 
     {/* Include the rest of sections, some with passed MDX data */}
     <About nav='About' id='about' />
     <Services nav='Services' id='services' />
     <Hire id='hire' />
-    <Portfolio nav='Portfolio' id='portfolio' data={portfolioData} />
     <Testimonials nav='Clients' id='testimonials' data={testimonialsData} />
     <Blog nav='Blog' id='blog' data={blogData} />
     <Contact nav='Contact' id='contact' />
@@ -36,7 +34,7 @@ const HomeImage = ({ portfolioData, testimonialsData, blogData }) => (
   </ScrollWrapper>
 )
 
-export default HomeImage
+export default HomeVideo
 
 /*
 This is a special Next.js function that allows fetching data
@@ -46,7 +44,6 @@ Read more: https://nextjs.org/docs/pages/building-your-application/data-fetching
 */
 export const getStaticProps = async () => ({
   props: {
-    portfolioData: await parseAllMdx(PortfolioDataPath),
     testimonialsData: await parseAllMdx(testimonialsDataPath),
     blogData: await parseAllMdx(BlogDataPath),
   },
--- src/partials/about/index.jsx
+++ src/partials/about/index.jsx
@@ -1,19 +1,14 @@
-/*
-This is the About section
-*/
-
-import { Row, Col } from 'react-bootstrap'
+import { Col } from 'react-bootstrap'
 import SectionWrapper from 'root/src/components/section-wrapper'
 import Button from 'root/src/components/button'
 import aboutImg from 'root/public/partials/about/picture.jpg'
 import Image from 'next/image'
-import { Link as ScrollLink } from 'react-scroll'
 import styled from './style'
 
 const About = (props) => (
   <SectionWrapper
     css={styled.About}
-    headerData={{ title: 'About me', description: 'Get to know me' }}
+    headerData={{ title: 'About me', description: '' }}
     {...props}
   >
     <div className='row align-items-center'>
@@ -39,7 +34,7 @@ const About = (props) => (
           I&apos;m Alex Smith, a visual UX/UI Designer and Web Developer
         </h2>
 
-        <div className='_description'>
+        <div className='_description mb-5'>
           <p>
             I am a freelancer based in the United Kingdom and i have been
             building noteworthy UX/UI designs and websites for years, which
@@ -48,48 +43,19 @@ const About = (props) => (
             product evolution helps me prioritize tasks, iterate fast and
             deliver faster.
           </p>
+          <span>
+            I am a freelancer based in the United Kingdom and i have been
+            building noteworthy UX/UI designs and websites for years, which
+            comply with the latest design trends. I help convert a vision and an
+            idea into meaningful and useful products. Having a sharp eye for
+            product evolution helps me prioritize tasks, iterate fast and
+            deliver faster.
+          </span>
         </div>
 
-        <address className='_address'>
-          <Row>
-            <Col className='_info' xs='12' md='6'>
-              <span>Name:</span>
-              <p>Alex Smith</p>
-            </Col>
-            <Col className='_info' xs='12' md='6'>
-              <span>Email:</span>
-              <p>
-                <a href='alex@example.com'>alex@example.com</a>
-              </p>
-            </Col>
-          </Row>
-          <Row>
-            <Col className='_info' xs='12' md='6'>
-              <span>Age:</span>
-              <p>21</p>
-            </Col>
-            <Col className='_info' xs='12' md='6'>
-              <span>From:</span>
-              <p>Liverpool, UK</p>
-            </Col>
-          </Row>
-        </address>
-
         {/* Buttons part */}
-        <Button className='_button' href='/partials/about/cv.pdf' download>
+        <Button className='_button mt-3' href='/partials/about/cv.pdf' download>
           Download CV
-        </Button>
-
-        <Button
-          as={ScrollLink}
-          to={'portfolio'}
-          spy={true}
-          smooth={true}
-          duration={600}
-          className='_button'
-          variant='light'
-        >
-          My Work
         </Button>
       </Col>
     </div>
