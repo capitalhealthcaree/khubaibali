f42a8263efa4ced239afe3306303006d6bf7dee5
capitalhealthcaree <premierpatientappointment@gmail.com>
2023-11-06 13:56:22 -0800

    add new theme

--- src/partials/services/index.jsx
+++ src/partials/services/index.jsx
@@ -1,25 +1,27 @@
+/*
+This is the services section
+*/
+
 import { Row, Col } from 'react-bootstrap'
+import * as icons from '@swiftcarrot/react-ionicons'
 import SectionWrapper from 'root/src/components/section-wrapper'
 import styled from './style'
 
-const experties = [
-  {
-    title: 'Design Trends',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-  },
-  {
-    title: 'Design Trends',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-  },
-  {
-    title: 'Design Trends',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-  },
-  {
-    title: 'Design Trends',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-  },
-]
+// Single service component
+const SingleService = (props) => {
+  // Destructure props
+  const { cols, Icon, description, title } = props
+
+  return (
+    <Col {...cols}>
+      <div css={styled.Service}>
+        <Icon className='_icon' />
+        <h6 className='_title'>{title}</h6>
+        <p className='_description'>{description}</p>
+      </div>
+    </Col>
+  )
+}
 
 // Services component
 const Services = (props) => (
@@ -27,23 +29,49 @@ const Services = (props) => (
     css={styled.Services}
     altBg={true}
     headerData={{
-      title: 'My Expertise',
-      description: '',
+      title: 'My Services',
+      description: 'Services i offer to my clients',
     }}
     {...props}
   >
     {/* List of services */}
     <Row>
-      {experties.map((item) => (
-        <>
-          <Col xs='12' md='6' lg='4'>
-            <div css={styled.Service}>
-              <h6 className='_title'>{item.title}</h6>
-              <p className='_description'>{item.description}</p>
-            </div>
-          </Col>
-        </>
-      ))}
+      <SingleService
+        Icon={icons.LogoCss3}
+        title='Design Trends'
+        description='Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+        cols={{ xs: '12', md: '6', lg: '4' }}
+      />
+      <SingleService
+        Icon={icons.Images}
+        title='PSD Design'
+        description='Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+        cols={{ xs: '12', md: '6', lg: '4' }}
+      />
+      <SingleService
+        Icon={icons.LogoIonic}
+        title='Customer Support'
+        description='Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+        cols={{ xs: '12', md: '6', lg: '4' }}
+      />
+      <SingleService
+        Icon={icons.LogoWordpress}
+        title='Web Development'
+        description='Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+        cols={{ xs: '12', md: '6', lg: '4' }}
+      />
+      <SingleService
+        Icon={icons.PhonePortraitSharp}
+        title='Fully Responsive'
+        description='Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+        cols={{ xs: '12', md: '6', lg: '4' }}
+      />
+      <SingleService
+        Icon={icons.RocketSharp}
+        title='Branding'
+        description='Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+        cols={{ xs: '12', md: '6', lg: '4' }}
+      />
     </Row>
   </SectionWrapper>
 )
