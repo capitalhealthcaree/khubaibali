7cd2c00a9767a47d09eb815068587d1d82aeebf6
capitalhealthcaree <premierpatientappointment@gmail.com>
2023-11-06 18:00:29 -0800

    vvffgg

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 /*
-This is the home page but with just an image background for the Hero section
+This is the home page but with a video background for the Hero section
 */
 
 import ScrollWrapper from 'root/src/components/scroll-wrapper'
@@ -7,47 +7,28 @@ import Hero from 'root/src/partials/hero'
 import About from 'root/src/partials/about'
 import Services from 'root/src/partials/services'
 import Hire from 'root/src/partials/hire'
-import Portfolio, { PortfolioDataPath } from 'root/src/partials/portfolio'
-import Blog, { BlogDataPath } from 'root/src/partials/blog'
+import Blog from 'root/src/partials/blog'
 import Contact from 'root/src/partials/contact'
 import Footer from 'root/src/partials/footer'
-import parseAllMdx from 'root/src/lib/parseAllMdx'
 import Metadata from 'root/src/metadata'
-import Testimonials, { testimonialsDataPath } from '../partials/testimonials'
 
-const HomeImage = ({ portfolioData, testimonialsData, blogData }) => (
+const HomeVideo = () => (
   /* Wrap all sections within a scroll-wrapper that adds a functional navbar/sidebar feature */
   <ScrollWrapper>
     {/* Include website metadata */}
     <Metadata />
 
-    {/* Hero section with an image background */}
-    <Hero nav='Home' id='home' variant='image' />
+    {/* Hero section with a video background */}
+    <Hero nav='Home' id='home' variant='video' />
 
     {/* Include the rest of sections, some with passed MDX data */}
     <About nav='About' id='about' />
     <Services nav='Services' id='services' />
     <Hire id='hire' />
-    <Portfolio nav='Portfolio' id='portfolio' data={portfolioData} />
-    <Testimonials nav='Clients' id='testimonials' data={testimonialsData} />
-    <Blog nav='Blog' id='blog' data={blogData} />
+    <Blog nav='Blog' id='blog' />
     <Contact nav='Contact' id='contact' />
     <Footer id='footer' />
   </ScrollWrapper>
 )
 
-export default HomeImage
-
-/*
-This is a special Next.js function that allows fetching data
-at build-time which is known as Static Site Generation (SSG).
-In this context it is retrieving data from MDX files to be passed to page sections
-Read more: https://nextjs.org/docs/pages/building-your-application/data-fetching/get-static-props
-*/
-export const getStaticProps = async () => ({
-  props: {
-    portfolioData: await parseAllMdx(PortfolioDataPath),
-    testimonialsData: await parseAllMdx(testimonialsDataPath),
-    blogData: await parseAllMdx(BlogDataPath),
-  },
-})
+export default HomeVideo
